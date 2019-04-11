8914c05c5d4cb9be44fef0aac1e371785072da32
joelcr23 <a20161442@pucp.edu.pe>
2019-04-10 20:39:13 -0500

    Estructura de paquetes

---
+++ pe/edu/pucp/IEPMariaAlvarado/model/bean/School.java
@@ -0,0 +1,14 @@
+public class School{
+	private String name;
+	private String address;
+	private Class classes;
+	private secretaries ArrayList<Secretary>;
+	private Auxiliar auxiliar;
+	private ArrayList<Parent> parents;
+	private ArrayList<Teacher> teachers;
+	private ExamsRepository exams; 
+	private Principal principal;
+	
+	
+	
+}
