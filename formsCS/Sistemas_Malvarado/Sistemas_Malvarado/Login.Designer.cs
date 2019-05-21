53754f1c545db9528a380cc08ddeb71806b21644
alulab14 <alulab14@inf.pucp.edu.pe>
2019-05-20 19:59:42 -0500

    sdfd

--- formsCS/Sistemas_Malvarado/Sistemas_Malvarado/Login.Designer.cs
+++ formsCS/Sistemas_Malvarado/Sistemas_Malvarado/Login.Designer.cs
@@ -169,7 +169,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(30, 44);
+            this.pictureBox1.Location = new System.Drawing.Point(23, 44);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(205, 213);
             this.pictureBox1.TabIndex = 10;
